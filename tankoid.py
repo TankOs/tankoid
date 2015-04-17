c407da80f3b9dff79b523d590d69bfc5b271350f
Stefan Schindler <stefan@boxbox.org>
2015-04-17 13:35:31 +0200

    Ball + movement.

--- tankoid.py
+++ tankoid.py
@@ -2,11 +2,41 @@
 
 from sfml import sf
 
+import math
+
+PADDLE_SIZE = sf.Vector2(140, 30)
+PADDLE_SPEED = 750
+PADDLE_COLOR = sf.Color.WHITE
+BRICK_SIZE = sf.Vector2(50, 25)
+BRICK_TYPES = {
+  0: sf.Color.RED,
+  1: sf.Color.BLUE,
+  2: sf.Color.GREEN,
+  3: sf.Color.WHITE,
+}
+BRICK_GAP_WIDTH = 12
+CLEAR_COLOR = sf.Color(0, 128, 128)
+LEVEL_SIZE = sf.Vector2(10, 8)
+BALL_RADIUS = 10.0
+BALL_COLOR = sf.Color.WHITE
+BALL_SPEED = 500.0
+
+def normalized_vector(vector):
+  length = math.sqrt(vector.x * vector.x + vector.y * vector.y)
+  return sf.Vector2(vector.x / length, vector.y / length)
+
 def create_shadow(shape, distance=3):
-  shadow = sf.RectangleShape(shape.size)
+  if type(shape) is sf.RectangleShape:
+    shadow = sf.RectangleShape(shape.global_bounds.size)
+  elif type(shape) is sf.CircleShape:
+    shadow = sf.CircleShape(shape.radius)
+  else:
+    assert(False and "Invalid shape type.")
+
   shadow.position = shape.position + sf.Vector2(distance, distance)
   shadow.origin = shape.origin
   shadow.fill_color = sf.Color(0, 0, 0, 50)
+
   return shadow
 
 def load_bricks(path, size, brick_type_pool, brick_size, gap_width,):
@@ -46,20 +76,6 @@ def load_bricks(path, size, brick_type_pool, brick_size, gap_width,):
 
   return bricks
 
-PADDLE_SIZE = sf.Vector2(140, 30)
-PADDLE_SPEED = 750
-PADDLE_COLOR = sf.Color.WHITE
-BRICK_SIZE = sf.Vector2(50, 25)
-BRICK_TYPES = {
-  0: sf.Color.RED,
-  1: sf.Color.BLUE,
-  2: sf.Color.GREEN,
-  3: sf.Color.WHITE,
-}
-BRICK_GAP_WIDTH = 12
-CLEAR_COLOR = sf.Color(0, 128, 128)
-LEVEL_SIZE = sf.Vector2(10, 8)
-
 window = sf.RenderWindow(sf.VideoMode(1024, 768), "Tankoid (Python)", 0)
 run = True
 
@@ -91,6 +107,14 @@ paddle.position = sf.Vector2(
   window.size.y - (paddle.size.y - paddle.origin.y) - 20,
 )
 
+ball = sf.CircleShape(BALL_RADIUS)
+ball.origin = ball.global_bounds.size / 2
+ball.fill_color = BALL_COLOR
+ball.position = paddle.position - sf.Vector2(0, paddle.size.y)
+
+ball_velocity = sf.Vector2(0, 0)
+ball_attached_to_paddle = True
+
 frame_timer = sf.Clock()
 
 while run is True:
@@ -99,25 +123,41 @@ while run is True:
       if event.code == sf.Keyboard.ESCAPE:
         run = False
 
+      elif event.code == sf.Keyboard.SPACE:
+        if ball_attached_to_paddle is True:
+          ball_attached_to_paddle = False
+          ball_velocity = normalized_vector(sf.Vector2(1, -1)) * BALL_SPEED
+
   frametime = frame_timer.restart()
 
   # Paddle velocity.
-  paddle_velocity = sf.Vector2(
-    (
-      (sf.Keyboard.is_key_pressed(sf.Keyboard.LEFT) and -1) +
-      (sf.Keyboard.is_key_pressed(sf.Keyboard.RIGHT) and 1)
-    ),
-    0
+  move_left = (
+    sf.Keyboard.is_key_pressed(sf.Keyboard.LEFT) |
+    sf.Keyboard.is_key_pressed(sf.Keyboard.A)
   )
+  move_right = (
+    sf.Keyboard.is_key_pressed(sf.Keyboard.RIGHT) |
+    sf.Keyboard.is_key_pressed(sf.Keyboard.D)
+  )
+  paddle_velocity = sf.Vector2((move_left and -1) + (move_right and 1), 0)
+  paddle_velocity *= PADDLE_SPEED
+  paddle.position += paddle_velocity * frametime.seconds
 
-  paddle.position += paddle_velocity * PADDLE_SPEED * frametime.seconds
+  if ball_attached_to_paddle is True:
+    ball.position = paddle.position - sf.Vector2(0, paddle.size.y)
+  else:
+    ball.position += ball_velocity * frametime.seconds
 
   window.clear(CLEAR_COLOR)
-  window.draw(create_shadow(paddle))
-  window.draw(paddle)
 
   for brick in bricks:
     window.draw(create_shadow(brick))
     window.draw(brick)
+
+  window.draw(create_shadow(paddle))
+  window.draw(paddle)
+
+  window.draw(create_shadow(ball))
+  window.draw(ball)
 
   window.display()
